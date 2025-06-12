830fb5547b8f3a7b537e56c1fe15050439d47ba6
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-12 21:28:10 +0330

    🟨FRAMER___MOTION🟨

--- app/framer-motion-1/page.tsx
+++ app/framer-motion-1/page.tsx
@@ -9,7 +9,7 @@ const FramerMotionPage1 = () => {
 
   const AnimationVariants = {
     hidden: { opacity: 0, x: -100 },
-    visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+    visible: { opacity: 1, x: 50, transition: { duration: 2 , ease: 'backInOut' } },
     exit: { opacity: 0, x: 100, transition: { duration: 0.5 } },
   };
 
