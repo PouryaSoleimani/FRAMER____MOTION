1104df2ccce0e9fd811756323f677123a7886d36
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-13 13:18:34 +0330

    🟨FRAMER___MOTION🟨

--- app/framer-motion-1/page.tsx
+++ app/framer-motion-1/page.tsx
@@ -9,10 +9,9 @@ const FramerMotionPage1 = () => {
 
   // ^ VARIANTS
   const AnimationVariants = {
-    hidden: { opacity: 0, y: -100 },
-    visible: { opacity: 1, y: 0 },
-    transition: { duration: 2, ease: 'backInOut' },
-    exit: { opacity: 0, y: -100, transition: { duration: 1 } },
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1.5, ease: 'backOut' } },
+    exit: { opacity: 0, x: -100, transition: { duration: 1 } },
   };
 
   function showHandler() {
@@ -22,15 +21,21 @@ const FramerMotionPage1 = () => {
   // ^ RETURN
   return (
     <section className="*:rounded-lg *:m-3 overflow-x-hidden p-6">
-      <motion.div id="BOX___1" className="bg-orange-500 size-32" initial={{ scale: 0 }} animate={{ scale: 1, rotate: 360 }}></motion.div>
+      <motion.div id="BOX___1" className="bg-orange-500 size-32" initial={{ scale: 0 }} animate={{ scale: 1, rotate: 180, transition: { ease: 'backOut', duration: 2 } }}></motion.div>
       <motion.div variants={AnimationVariants} id="BOX___1" className="bg-red-500 size-32" initial="hidden" animate="visible" whileTap={{ scale: 0.95 }} whileHover={{ scale: 1.03 }}></motion.div>
       <motion.div id="BOX___1" className="bg-violet-500 size-32" initial={{ rotate: 0 }} animate={{ rotate: 360, x: 350 }} transition={{ duration: 2, ease: 'backOut', bounce: 0.2, delay: 0.5 }}></motion.div>
-      <AnimatePresence>{show ? <motion.div exit={{ opacity: 0 }} initial={{ opacity: 1 }} transition={{ duration: 0.5 }} className="bg-yellow-500 size-32" /> : null}</AnimatePresence>
+      {/* //^ AnimatePresence ^// */}
+      <AnimatePresence>{show ? <motion.div exit={AnimationVariants.exit} initial={AnimationVariants.hidden} className="bg-yellow-500 size-32" /> : null}</AnimatePresence>
       <button onClick={showHandler} className="text-white px-4 py-2 bg-emerald-600">
         TOGGLE
       </button>
+
+      {/* //^ VARIANT ^// */}
       <Variants />
+      {/* //^ LONG DIV ^// */}
       <LongDiv />
+
+      {/* //^ ON SCROLL ANIMATION */}
       <motion.div className="bg-[#ff0088] fixed -top-2 left-0 right-0 h-4" id="scroll-indicator" style={{ scaleX: scrollYProgress, originX: 0 }} />
       <motion.div id="BOX___1" className="bg-rose-500 size-32" initial={{ opacity: 0 }} whileInView={{ opacity: 1, rotate: 180 }} transition={{ duration: 1.2, ease: 'backOut', bounce: 0.2, delay: 0.3 }}></motion.div>
     </section>
