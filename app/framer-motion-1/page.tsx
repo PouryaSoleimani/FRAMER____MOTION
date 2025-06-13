3de33186d553d061692bf67599f8f0c37d028122
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-13 13:23:21 +0330

    🟨FRAMER___MOTION🟨

--- app/framer-motion-1/page.tsx
+++ app/framer-motion-1/page.tsx
@@ -25,7 +25,7 @@ const FramerMotionPage1 = () => {
       <motion.div variants={AnimationVariants} id="BOX___1" className="bg-red-500 size-32" initial="hidden" animate="visible" whileTap={{ scale: 0.95 }} whileHover={{ scale: 1.03 }}></motion.div>
       <motion.div id="BOX___1" className="bg-violet-500 size-32" initial={{ rotate: 0 }} animate={{ rotate: 360, x: 350 }} transition={{ duration: 2, ease: 'backOut', bounce: 0.2, delay: 0.5 }}></motion.div>
       {/* //^ AnimatePresence ^// */}
-      <AnimatePresence>{show ? <motion.div exit={AnimationVariants.exit} initial={AnimationVariants.hidden} className="bg-yellow-500 size-32" /> : null}</AnimatePresence>
+      <AnimatePresence>{show ? <motion.div exit={{ opacity: 0 }} initial={{ opacity: 1 }} transition={{ duration: 0.5 }} className="bg-yellow-500 size-32" /> : null}</AnimatePresence>
       <button onClick={showHandler} className="text-white px-4 py-2 bg-emerald-600">
         TOGGLE
       </button>
