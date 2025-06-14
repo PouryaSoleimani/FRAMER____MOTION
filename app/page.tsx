0b60363a68a78dbc10974b69a52520a8059ab1cb
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-14 22:24:22 +0330

    🟨 FRAMER___MOTION 🟨

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 // import DropDownMenu from '@/components/DropDownMenu';
 import MotionNavbar from '@/components/MotionNavbar';
 import FramerMotionPage1 from './framer-motion-1/page';
+import BoxesComponent from '@/components/4Boxes';
 
 export default function Home() {
   return (
     <div className="w-full h-screen p-8 *:rounded-lg">
       <MotionNavbar />
+      <BoxesComponent />
       <FramerMotionPage1 />
       {/* <DropDownMenu /> */}
     </div>
---
+++ components/4Boxes.tsx
@@ -0,0 +1,41 @@
+"use client"
+import React from 'react'
+import { motion } from 'motion/react'
+
+const BoxesComponent = () => {
+    const ParentVariants = {
+        hidden: {
+            y: -100,
+            opacity: 0
+        },
+        visible: {
+            y: 0,
+            opacity: 1,
+            transition: { duration: 1, when: "beforeChildren", staggerChildren: 0.1, ease: "backOut" }
+        }
+    }
+
+    const ChildVariants = {
+        hidden: { opacity: 0, y: -90 },
+        visible: { opacity: 1, y: 0 }
+    }
+
+    return (
+        <div className='w-full bg-zinc-300 my-3 p-4'>
+            <motion.div
+                className='flex flex-wrap gap-4 justify-evenly'
+                variants={ParentVariants}
+                initial="hidden"
+                animate="visible"
+            >
+                <motion.span className='w-1/6 h-32 rounded-xl bg-red-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-1/6 h-32 rounded-xl bg-blue-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-1/6 h-32 rounded-xl bg-orange-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-1/6 h-32 rounded-xl bg-indigo-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-1/6 h-32 rounded-xl bg-teal-600' variants={ChildVariants}></motion.span>
+            </motion.div>
+        </div>
+    )
+}
+
+export default BoxesComponent
