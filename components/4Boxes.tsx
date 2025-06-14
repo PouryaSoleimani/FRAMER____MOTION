998e87bdbe4620efee7ebe0b975f130089239624
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-15 00:10:51 +0330

    🟨 FRAMER___MOTION 🟨

--- components/4Boxes.tsx
+++ components/4Boxes.tsx
@@ -4,10 +4,7 @@ import { motion } from 'motion/react'
 
 const BoxesComponent = () => {
     const ParentVariants = {
-        hidden: {
-            y: -100,
-            opacity: 0
-        },
+        hidden: { y: -100, opacity: 0 },
         visible: {
             y: 0,
             opacity: 1,
@@ -23,16 +20,16 @@ const BoxesComponent = () => {
     return (
         <div className='w-full bg-zinc-300 my-3 p-4'>
             <motion.div
-                className='flex flex-wrap gap-4 justify-evenly'
+                className='flex flex-wrap gap-1 justify-evenly'
                 variants={ParentVariants}
                 initial="hidden"
                 animate="visible"
             >
-                <motion.span className='w-1/6 h-44 rounded-xl bg-red-600' variants={ChildVariants}></motion.span>
-                <motion.span className='w-1/6 h-44 rounded-xl bg-blue-600' variants={ChildVariants}></motion.span>
-                <motion.span className='w-1/6 h-44 rounded-xl bg-orange-600' variants={ChildVariants}></motion.span>
-                <motion.span className='w-1/6 h-44 rounded-xl bg-indigo-600' variants={ChildVariants}></motion.span>
-                <motion.span className='w-1/6 h-44 rounded-xl bg-teal-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-96 h-36 rounded-xl bg-red-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-96 h-36 rounded-xl bg-blue-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-96 h-36 rounded-xl bg-orange-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-96 h-36 rounded-xl bg-indigo-600' variants={ChildVariants}></motion.span>
+                <motion.span className='w-96 h-36 rounded-xl bg-teal-600' variants={ChildVariants}></motion.span>
             </motion.div>
         </div>
     )
