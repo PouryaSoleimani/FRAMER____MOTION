aaf34e555a03b40b09cb058e7dfedcfc7eb1da49
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-18 23:15:40 +0330

    REFACTOR

--- components/SharedLayout4.tsx
+++ components/SharedLayout4.tsx
@@ -10,9 +10,7 @@ const SharedLayout4 = () => {
         STICK TO TOP
       </button>
       <motion.div layout className={`navbar bg-black/80 rounded-box backdrop-blur-2xl items-center justify-evenly ${isSticky ? '*:w-fit' : ''}`}>
-        <motion.span layout className={`btn text-black btn-secondary w-1/6`}>
-          HOME
-        </motion.span>
+<motion.span layout className={`btn text-black btn-secondary w-1/6`}> HOME </motion.span>
         <motion.span layout className={`btn text-black btn-secondary w-1/6`}>
           GALLERY
         </motion.span>
