d61ce129c49aa968dc4d9d802f4b50f8c501fae4
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-06-18 22:34:39 +0330

    🟨FRAMER___MOTION🟨

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import SharedLayout4 from '@/components/SharedLayout4';
 export default function Home() {
   return (
     <div className="w-full h-screen p-8 *:rounded-lg">
-      <h2 className="bg-stone-400 text-3xl font-black text-center p-3 text-black my-4">STAGGER</h2> 
+      <h2 className="bg-stone-400 text-3xl font-black text-center p-3 text-black my-4">STAGGER</h2>
       <MotionNavbar />
       <BoxesComponent />
       <CirclesCompoent />
@@ -30,7 +30,6 @@ export default function Home() {
       <SharedLayout4 />
       <h2 className="bg-stone-400 text-3xl font-black text-center p-3 text-green-900 my-4">SIMPLE ANIMATIONS</h2>
       <FramerMotionPage1 />
-
       {/* <DropDownMenu /> */}
     </div>
   );
